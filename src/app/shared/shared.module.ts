6b4992ffd553f17f76568166bb198a0974f00354
Abdesselem Idani <abdesselem.idani@gmail.com>
2020-10-11 15:00:40 +0100

    feat top nav menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MaterialModule } from '../materials';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [NavBarComponent],
   imports: [
     MaterialModule,
+    RouterModule,
     CommonModule
   ],
   exports: [
