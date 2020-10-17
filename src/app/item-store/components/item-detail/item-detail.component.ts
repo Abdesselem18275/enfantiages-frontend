68d8bceb3a4945218efc7ebc16b5249fa61b6d9e
abesselem18275 <abesselem.idani@gmail.com>
2020-10-17 19:34:30 +0100

    style  styled item-detail page

--- src/app/item-store/components/item-detail/item-detail.component.ts
+++ src/app/item-store/components/item-detail/item-detail.component.ts
@@ -4,13 +4,18 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Item } from 'src/app/core/models/item-models';
 import { DialogHandlerService } from '../../../shared/service/dialog-handler.service';
-
+const ITEM_DETAIL_META :{icon:string,title:string,elements: {key:string,label:string}[]}[] = [
+  {icon:'qr_code',title:'IDENTIFICATION',
+  elements:[
+    {key:'reference',label:'Reference'},{key:'label',label:'Label'},{key:'status',label:'Status'}]}
+]
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.scss']
 })
 export class ItemDetailComponent  {
+  item_detail_meta = ITEM_DETAIL_META
   item$ : Observable<Item>
   constructor(private dhs: DialogHandlerService,route : ActivatedRoute) {
     this.item$ = route.data.pipe(map((x:Data) =>x.itemDetail))
@@ -18,5 +23,9 @@ export class ItemDetailComponent  {
 
    openSellDialog(item: Item) {
      this.dhs.openSellDialog(item)
+  }
+
+  getValue() {
+    
   }
 }
