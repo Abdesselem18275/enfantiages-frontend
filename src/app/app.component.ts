cf10aadd921c35aad09957f6697a5ff320023ab8
Abdesselem Idani <abdesselem.idani@gmail.com>
2020-10-19 21:15:19 +0100

    style : styled ItemDeposit & New customer form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { MatDrawer } from '@angular/material/sidenav';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'enfantiages';
   @ViewChild('drawer') drawer : MatDrawer
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(private router : Router,iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
         iconRegistry.addSvgIcon('under-construction-page', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/under-construction-page.svg'))
 
+  }
+  ngAfterViewInit(): void {
+    this.router.events.subscribe(() => {
+      this.drawer.close()
+   });
   }
   toggleSideNav() {
     this.drawer.toggle()
