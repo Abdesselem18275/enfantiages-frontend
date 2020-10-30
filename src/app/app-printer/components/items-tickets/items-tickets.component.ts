c2ba3061cbb95d0d0cba097276d4e49c0131a91a
abesselem18275 <abesselem.idani@gmail.com>
2020-10-30 13:02:27 +0100

    fix tickets can now be printed safely on multiple pages

--- src/app/app-printer/components/items-tickets/items-tickets.component.ts
+++ src/app/app-printer/components/items-tickets/items-tickets.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, retry, tap } from 'rxjs/operators';
 import { Item } from 'src/app/core/models/item-models';
 import { ItemStoreStateService } from 'src/app/item-store/service/item-store-state.service';
+import { AppDataService } from 'src/app/shared/service/app-data.service';
+const ITEMS_PER_PAGE = 27
 
 @Component({
   selector: 'app-items-tickets',
@@ -10,11 +13,21 @@ import { ItemStoreStateService } from 'src/app/item-store/service/item-store-sta
   styleUrls: ['./items-tickets.component.scss']
 })
 export class ItemsTicketsComponent implements OnInit {
-  itemTickets$  :Observable<Item[]>
-  constructor(private iss :ItemStoreStateService) { 
-    this.itemTickets$ = this.iss.selectedItems
+  itemTickets$  :Observable<Map<number,Item[]>>
+  itemsCount$ : Observable<number>
+  constructor(private ads : AppDataService,private iss :ItemStoreStateService) { 
+    this.itemsCount$  = this.iss.ItemsCount
+    this.itemTickets$ = this.iss.selectedItems.pipe(
+      map((items:Item[] )=> (items.reduce(
+        (acc: Map<number,Item[]>, currentValue: Item, currentIndex: number) => 
+      { 
+        const pageNumber = Math.floor(currentIndex/ITEMS_PER_PAGE) +1
+        return currentIndex % ITEMS_PER_PAGE === 0 ? 
+        acc.set(pageNumber,[currentValue]):
+        acc.set(pageNumber,acc.get(pageNumber).concat([currentValue]))
+      },new Map<number,Item[]>())) )
+    )
   }
-
   ngOnInit(): void {
   }
   print():void {
